96924846e50b9a82fee835f0a142507f85de42e2
mkytaran <novak.michal@gmail.com>
2026-09-08 10:49:19 +0200

    úpravy pro instalaci pwa

--- sw.js
+++ sw.js
@@ -1,7 +1,41 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Nainstalován');
+const CACHE_NAME = 'zus-rozvrh-v2.0';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-    // Projít testem instalace stačí i prázdný fetch event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Požadavky na Google Apps Script neukládáme do service worker cache
+  if (event.request.url.includes('script.google.com')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
 });
